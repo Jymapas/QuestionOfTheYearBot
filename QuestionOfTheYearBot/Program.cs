a533972ba745f638f3d3b1cecc8f9ad60d241798
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2024-11-18 10:53:20 +0500

    feat: add basic responce processing

--- QuestionOfTheYearBot/Program.cs
+++ QuestionOfTheYearBot/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var apiUrl = "https://gotquestions.online/api/question/395284/";
+var client = new HttpClient();
+var response = await client.GetAsync(apiUrl);
+
+if (response.IsSuccessStatusCode)
+{
+    var json = await response.Content.ReadAsStringAsync();
+    var question = JsonSerializer.Deserialize<Question>(json);
+
+    Console.WriteLine(question.text);
+}
+else
+{
+    Console.WriteLine("Failed to receive data");
+}
+
+class Question
+{
+    public string text { get; set; }
+}
